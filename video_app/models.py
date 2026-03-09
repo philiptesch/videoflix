afe9142f6ea3a276163c22326924a6a4d53ab1e6
Philip <philiptesch99@gmail.com>
2026-03-09 22:34:55 +0100

    video to hls

--- video_app/models.py
+++ video_app/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+CATEGORY_CHOICES = [
+    ('Anime', 'Anime'),
+    ('Action', 'Action'),
+    ('Comedy', 'Comedy'),
+    ('Drama', 'Drama'),
+    ('Horror', 'Horror'),
+    ('Sci-Fi', 'Sci-Fi'),
+    ('Documentary', 'Documentary'),
+    ('Animation', 'Animation'),
+    ('Thriller', 'Thriller'),
+    ('Romance', 'Romance'),
+    ('Fantasy', 'Fantasy'),
+]
+
+
 class Video(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200)
     description = models.TextField()
     thumbnail_url = models.FileField(upload_to='thumbnails/', blank=True, null=True)
-    category= models.CharField(max_length=50, default='Uncategorized')
+    category= models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='Action')
     video_file = models.FileField(upload_to='video/', blank=True, null=True)
 
 
--- video_app/tasks.py
+++ video_app/tasks.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 from django.core.files import File
-
+resolutions = [("480", "480p"), ("720", "720p"), ("1080", "1080p")]
 
 def save_new_video_path(instance, id):
     old_path = instance.video_file.path 
@@ -9,18 +9,21 @@ def save_new_video_path(instance, id):
     old = os.path.dirname(old_path)
     new_path = os.path.join(old, new_name)
     os.rename(old_path, new_path)
-    convert_Video(instance, new_path, new_name)
+
+    for  height, res in resolutions:
+        
+        convert_Video(instance, new_path, res, height)
 
 
-def convert_Video(id, new_path, new_name):
-    target_dir = os.path.join('media', 'video', '480p', str(id))
+def convert_Video(instance, new_path, res, height):
+    target_dir = os.path.join('media', 'video', res, str(instance.id))
     os.makedirs(target_dir, exist_ok=True)
     playlist_path = os.path.join(target_dir, "index.m3u8")
     segment_path = os.path.join(target_dir, "segment_%03d.ts")
 
     ffmpeg_command = [
         '-i', new_path,
-        '-vf', 'scale=854:480', 
+        '-vf', f'scale=854:{height}', 
         '-c:v', 'libx264',
         '-preset', 'medium',
         '-crf', '23',
@@ -29,6 +32,7 @@ def convert_Video(id, new_path, new_name):
         '-f', 'hls',
         '-hls_time', '6',
         '-hls_playlist_type', 'vod',
+        '-hls_base_url', f"media/video/{instance.id}/{res}"
         '-hls_segment_filename', segment_path,
         playlist_path
     ]
