5fa3ab880394c1320e40e1b094aa270004debede
Philip <philiptesch99@gmail.com>
2026-03-17 15:50:29 +0100

    add video_app views and urls

--- video_app/api/urls.py
+++ video_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ListVideoView
+from .views import ListVideoView, VideoResolutionView, VideoSegmentView
 
 urlpatterns = [
    path('video/', ListVideoView.as_view(), name='video_List'),
-
+   path('video/<int:movie_id>/<str:resolution>/index.m3u8', VideoResolutionView.as_view(), name='video_Resolution_List'),
+    path('video/<int:movie_id>/<str:resolution>/<str:segment>/', VideoSegmentView.as_view(), name='video_Segment_List'),
 ]
--- video_app/api/views.py
+++ video_app/api/views.py
@@ -7,6 +7,10 @@ from django.shortcuts import get_object_or_404
 import os
 from rest_framework.response import Response
 from rest_framework import status
+from django.conf import settings
+from pathlib import Path
+from django.http import HttpResponse
+from django.http import FileResponse, Http404
 
 class ListVideoView(ListAPIView):
     permission_classes = [IsAuthenticated]
@@ -15,19 +19,56 @@ class ListVideoView(ListAPIView):
 
 
 class VideoResolutionView(APIView):
-    serializer_class = VideoListSeralizers
-    
+    permission_classes = [IsAuthenticated]
     
     def get(self, request, *args, **kwargs):
 
-        pk = self.kwargs.get('pk')
+        movie_id = self.kwargs.get('movie_id')
         resolution = self.kwargs.get('resolution')
-        obj = get_object_or_404(Video, pk=pk)
-        path = f"media/video/{resolution}/{str(pk)}"
+
+        video_exists = Video.objects.filter(pk=movie_id).exists()
+        if not video_exists:
+            return Response({"detail": "Video not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        path = Path(settings.MEDIA_ROOT)  / 'video' / resolution / str(movie_id) / 'index.m3u8'
+
 
         if not os.path.exists(path):
             return Response({"detail": "Playlist not found"},status=status.HTTP_404_NOT_FOUND)
 
+        with open(path, 'r', encoding="utf-8") as file:
+            manifest_content = file.read()
+
+        return HttpResponse(
+            content=manifest_content, 
+            content_type='application/vnd.apple.mpegurl'
+        )
+
+
+class VideoSegmentView(APIView):
+    permission_classes = [IsAuthenticated]
+
+
+    def get(self, request, *args, **kwargs):
+
+        movie_id = self.kwargs.get('movie_id')
+        resolution = self.kwargs.get('resolution')
+        segment = self.kwargs.get('segment')
+
+        segment_file = os.path.basename(segment)
+
+        video_exists = Video.objects.filter(pk=movie_id).exists()
+        if not video_exists:
+            return Response({"detail": "Video not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if not segment_file.endswith(".ts"):
+            return Response({"detail": "Invalid segment type"}, status=400)
+        
+        path = Path(settings.MEDIA_ROOT) / 'video' / resolution / str(movie_id) / str(segment)
+                
+        if os.path.exists(path):
+            return FileResponse(open(path, 'rb'), content_type='video/MP2T')
+        raise Http404("Segment not found")
 
 
 
